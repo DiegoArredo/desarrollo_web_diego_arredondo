441a029f6d6fd59b40f79f46bfbbd9b97d2d59ea
DiegoArredo <diego.arredo18@gmail.com>
2025-06-29 23:58:26 -0400

    Se reorganiza el proyecto y se crea una subcarpeta para el la app en flask y el microservicio de notas con spring boot

---
+++ spring_boot_microservicio_notas/m_notas/src/main/java/microservicio_notas/m_notas/MNotasApplication.java
@@ -0,0 +1,13 @@
+package microservicio_notas.m_notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MNotasApplication.class, args);
+	}
+
+}
---
+++ spring_boot_microservicio_notas/m_notas/src/test/java/microservicio_notas/m_notas/MNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicio_notas.m_notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
